3c848f5502a7d1298153e23fb8ffa0a121b05832
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-05-18 18:29:17 +0530

    Add files via upload

---
+++ CoreJava_2_from_OOPs/src/com/oops/exceptionhandling/ClassFornName.java
@@ -0,0 +1,11 @@
+package com.exceptionhandling;
+
+public class ClassFornName {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		// TODO Auto-generated method stub
+		
+		Class.forName("com.exceptionhandling.BusServiceMain");
+	}
+
+}
