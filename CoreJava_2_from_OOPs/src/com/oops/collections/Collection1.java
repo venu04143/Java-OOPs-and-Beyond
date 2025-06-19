4d6c310ab0e9fc3916aa492136f8b5265cd0444b
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-06-19 21:37:36 +0530

    Add files via upload

---
+++ CoreJava_2_from_OOPs/src/com/oops/collections/Collection1.java
@@ -0,0 +1,27 @@
+package com.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Collection1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Collections has Growable in Nature
+		
+		Collection cn = new ArrayList();
+		
+		cn.add(10);
+		cn.add(20);
+		cn.add(30);
+		cn.add(40);
+		cn.add("Venu");
+		cn.add("Gopal");
+		
+		System.out.println(cn);
+		
+
+	}
+
+}
