1ca50d4ef6b3af9e3e82ebe94d85c0f0683160bd
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-06-29 10:37:20 +0530

    Add files via upload

---
+++ CoreJava_2_from_OOPs/src/com/oops/collections/map/Map1.java
@@ -0,0 +1,33 @@
+package com.collections.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Map1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Map m = new HashMap();
+		
+		m.put(1, "Venu");
+		m.put(2, "Luckey");
+		m.put(3, "Pranay");
+		m.put(4, "Eswar");
+		
+		System.out.println(m);
+		
+
+		Map m1 = new HashMap();
+		
+		m1.put(102, "Venu");
+		m1.put(2, "Luckey");
+		m1.put(33, "Pranay");
+		m1.put(45, "Eswar");
+		
+		System.out.println(m1);
+		
+
+	}
+
+}
