a07ee7dedb19d48e51f2d50e164ce5a69272caae
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-07-02 12:02:25 +0530

    Add files via upload

---
+++ CoreJava_2_from_OOPs/src/com/oops/collections/problems/AnagramsFromListOfStrings.java
@@ -0,0 +1,24 @@
+package com.collections.problems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AnagramsFromListOfStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String [] arr = {"eat","tea","tan","ate","nat","bat"};
+		
+		List<String> str = new ArrayList<>(Arrays.asList(arr));
+		
+		System.out.println(str);
+		
+		
+		
+		
+
+	}
+
+}
